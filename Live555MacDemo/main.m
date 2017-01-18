e375b4443448be3d452840ac007d591c9fdb4d31
hangjw0322 <hangjw0322@thundersoft.com>
2017-01-18 10:20:36 +0800

    Initial Commit

---
+++ Live555MacDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Live555MacDemo
+//
+//  Created by huangjianwu on 2017/1/18.
+//  Copyright © 2017年 huangjianwu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
